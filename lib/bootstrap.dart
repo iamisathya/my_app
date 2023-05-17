2c08642af691e575a96b646f69403de1b0c76abc
Sathyanarayana AN <sathyanarayana.an@honeywell.com>
2023-05-17 14:15:37 +0530

    1st commit

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -20,6 +20,7 @@ class AppBlocObserver extends BlocObserver {
   }
 }
 
+//this is sample comment
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
